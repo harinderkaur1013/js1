98c3459ab34f96c389ccc9287f9cb96b2baec2f3
harinderkaur <kaurh1013@gmil.com>
2017-03-27 10:42:50 -0400

    js1

---
+++ js/app.js
@@ -0,0 +1,83 @@
+//1.
+/* var Age = prompt("Enter Your Age");
+if (Age <= 14) {
+    console.log("Not Allowed");
+} else if (Age == 50) {
+    console.log("Special Discount");
+} else if (Age > 14 && Age < 65) {
+    console.log("Regular Price");
+} else {
+    console.log("Invaild");
+}
+*/
+
+//2.
+/*
+var Age = prompt("Enter Your Age");
+if (Age == 14) {
+    console.log("coupon1");
+} else if (Age == 21) {
+    console.log("coupon2");
+} else if (Age == 30) {
+    console.log("coupon3");
+} else if (Age == 50) {
+    console.log("coupon4");
+} else {
+    console.log("No coupon")
+}
+*/
+
+//3.
+
+var Day = prompt("Enter a Day");
+if (Day == "Monday" || Day == "monday") {
+    console.log("Mondays,We serve Pasta");
+} else if (Day == "Friday" || Day == "friday") {
+    console.log("Fridays,we serve Tacos");
+} else {
+    console.log("Not Valid")
+}
+//4.
+var Year = prompt("Enter a Year");
+if (Year == "2009") {
+    console.log("Lock");
+}
+if (Year == "2010") {
+    console.log("last");
+} else if (Year == "2011") {
+    console.log("Jungle");
+} else if (Year == "2012") {
+    console.log("Book");
+} else if (Year == "2013") {
+    console.log("Birdman");
+} else if (Year == "2014") {
+    console.log("Slave");
+} else if (Year == "2015") {
+    console.log("Animals")
+} else if (Year == "2016") {
+    console.log("Spotlight")
+} else if (Year == "2017") {
+    console.log("Light")
+}
+//5
+var Grade = prompt("Enter the Grades"); {
+    if (Grade == "A++" || Grade == "a++") {
+        console.log("Excellent");
+    } else if (Grade == "A+") {
+        console.log("Good job");
+    } else if (Grade == "B") {
+        console.log("Good");
+    } else if (Grade == "C") {
+        console.log("Average");
+    }
+
+
+
+
+
+
+
+
+
+
+}
